09e5c61488f64e14d2171de41304c758fbec1c37
Peng Liu <pdupub@gmail.com>
2023-11-20 14:31:10 +0800

    comments to en

--- contract/Vote.sol
+++ contract/Vote.sol
@@ -4,72 +4,72 @@ import "./SafeMath.sol";
 import "./PermissionGroups.sol";
 
 /*
-    1. 管理员可以确定这次投票的开始提议时间，开始投票时间，结束时间。
-    2. 管理员可以确定提议所需的锁定金额等。
-    3. 任何人可以在开放提议后对于项目进行提议，提议需要锁定一定的maro，在投票完成后一律归还。
-    4. 任何人可以在投票开始后，对于支持的项目进行投票。
-    5. 在投票结束后，由管理员执行计票操作，对于本次的投票结果，按照投票地址此时的balance进行计票操作。
-    6. 管理员计票完成后，用户可以公开读取结果。
+    1. The administrator can determine the start proposal time, start voting time, and end time of this voting.
+    2. The administrator can determine the lock amount required for the proposal, etc.
+    3. Any address can propose a project after the proposal is opened. The proposal needs to lock a certain amount of maro, which will be returned after the voting is completed.
+    4. Any address can vote for supported projects after the voting begins.
+    5. After the voting ends, the administrator will perform the vote counting operation. For this voting result, the vote counting operation will be carried out according to the balance of the voting address at this time.
+    6. After the administrator completes vote counting, users can publicly read the results.
 */
 
 contract Vote is PermissionGroups {
     using SafeMath for uint;
 
     struct Proposal {
-        uint id;                    //  提议ID
-        address proposer;           //  提议地址
-        string content;             //  提议内容
-        uint deposit;               //  锁定押金
-        uint voteCount;             //  临时得票用于显示
-        address[] currentVoters;    //  投票人列表
-        bool disabled;              //  使得这个投票失效
-        mapping(address => uint) recs;          //  记录
+        uint id;                    //  proposal ID
+        address proposer;           //  proposer address
+        string content;             //  proposal content
+        uint deposit;               //  lock maro amount for proposal
+        uint voteCount;             //  vote count
+        address[] currentVoters;    //  voter address list
+        bool disabled;              //  disabled of proposal only by proposer
+        mapping(address => uint) recs;          //  records of address and vote
     }
 
 
     uint constant public decimals = 18;
-    string public name = "";        // 本次投票合约的名称，可以为空
-    uint public proposalID = 1;     // ID 从1开始，用0判断
+    string public name = "";        // name of current event
+    uint public proposalID = 1;     // proposal ID start from 1
 
-    uint public proposeStartTime = 0;   // 提案开始时间
-    uint public proposeEndTime = 0;     // 提案结束时间
-    uint public voteEndTime = 0;        // 投票结束时间
+    uint public proposeStartTime = 0;   // proposal start time
+    uint public proposeEndTime = 0;     // proposal end time & vote begin time
+    uint public voteEndTime = 0;        // vote end time
 
     uint public minProposeLockAmount = 10 ** decimals * 100000;
     uint public minVoteBalance = 10 ** decimals * 100;
 
-    mapping (uint => Proposal) public proposals;       //  所有提议
-    mapping (address => uint) public voteTarget;       //  某地址当前的目标提议ID
-    address[] public voters;                           //  所有参与投票的人
+    mapping (uint => Proposal) public proposals;       //  all proposal
+    mapping (address => uint) public voteTarget;       //  address => support proposal
+    address[] public voters;                           //  all address list
 
 
-    // p是总的序号，对应proposalID
-    // a是行为， 1 是提出提案， 2 是投票提案 3 退钱 4 修改提案内容 5 失效
-    // addr是对应的用户地址
-    // amount是对应的金额，提案的时候是锁定的押金，投票的时候是当前的balance
+    // p is proposalID
+    // a ia action， 1 create proposal 2 vote 3 refund 4 edit proposal content 5 disable proposal
+    // addr is user address
+    // amount lock amount for create proposal & balance for vote
     event M(uint indexed p, uint indexed a, address indexed addr,  uint amount);
 
-    // 初始化
+    // 
     function Vote(string _name) public {
         name = _name;
     }
     
-    // 设定时间点, 提议开始
+    //
     function setProposeStartTime(uint timestamp) public onlyAdmin {
         proposeStartTime = timestamp;
     }
 
-    // 设定时间，提议结束
+    //
     function setProposeEndTime(uint timestamp) public onlyAdmin {
         proposeEndTime = timestamp;
     }
 
-    // 设定时间，投票结束
+    //
     function setVoteEndTime(uint timestamp) public onlyAdmin {
         voteEndTime = timestamp;
     }
 
-    // 设定提议锁定的价格
+    //
     function setMinProposalLockAmount(uint amount) public onlyAdmin {
         minProposeLockAmount = 10 ** decimals * amount;
     }
@@ -78,7 +78,7 @@ contract Vote is PermissionGroups {
         minVoteBalance = 10 ** decimals * amount;
     }
 
-    // 提议
+    // create proposal
     function propose(string proposal) public payable {
         require(msg.value >= minProposeLockAmount);
         require(block.timestamp >= proposeStartTime && block.timestamp <= proposeEndTime);
@@ -99,7 +99,7 @@ contract Vote is PermissionGroups {
         proposalID += 1;
     }
 
-    // 修改提议
+    // edit proposal
     function editProposal(uint id, string proposal) public {
         require(block.timestamp >= proposeStartTime && block.timestamp <= proposeEndTime);
         require(msg.sender == proposals[id].proposer);
@@ -108,7 +108,7 @@ contract Vote is PermissionGroups {
 
     }
 
-    // disable提议
+    // disable proposal
     function disableProposal(uint id) public {
         require(block.timestamp >= proposeStartTime && block.timestamp <= proposeEndTime);
         require(msg.sender == proposals[id].proposer);
@@ -119,21 +119,21 @@ contract Vote is PermissionGroups {
         M(id, 5, msg.sender, 0);
     }
 
-    // 投票
+    // vote for proposal
     function vote(uint id) public {
         require(id>0 && id < proposalID);
         require(msg.sender.balance >= minVoteBalance);
         require(block.timestamp >= proposeEndTime && block.timestamp <= voteEndTime);
         require(proposals[id].disabled == false);
 
-        // 先检查这个地址是否投过票
+        // check voter address first
         if (voteTarget[msg.sender] > 0) {
             uint pID = voteTarget[msg.sender];
             uint vAmount = proposals[pID].recs[msg.sender];
             proposals[pID].voteCount = proposals[pID].voteCount.sub(vAmount);
             proposals[pID].recs[msg.sender] = 0;
-            // 这里可以忽略 voters中的记录
-            // 也忽略currentVoters中的记录
+            // ignore record in voters
+            // ignore record in currentVoters
         }
 
         voteTarget[msg.sender] = id;
@@ -145,7 +145,7 @@ contract Vote is PermissionGroups {
 
     }
 
-    // 计票, index 遍历voters
+    // counting on index of voters
     function counting(uint startIndex, uint len) public onlyAdmin {
         require(block.timestamp > voteEndTime);
         for (uint i = startIndex; i < startIndex + len; i++) {
@@ -158,7 +158,7 @@ contract Vote is PermissionGroups {
         }
     }
 
-    // 退款， index遍历proposals
+    // refund on index of proposals
     function refund(uint startIndex, uint len) public  {
         require(block.timestamp > proposeEndTime);
         for (uint i = startIndex; i < startIndex+len ; i++) {
@@ -174,6 +174,7 @@ contract Vote is PermissionGroups {
         }        
     }
 
+    // display all the records
     function displayRecs(uint id, uint startIndex, uint len) public view returns (address[] memory, uint[] memory) {
         address[] memory addrs = new address[](len);
         uint[] memory balance = new uint[](len);
